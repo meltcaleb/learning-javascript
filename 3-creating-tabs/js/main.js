3af068afb78c5e853ace2eb7fa29ca867eea44d3
meltcaleb <caleb.varoga@meltmedia.com>
2015-01-05 16:23:42 -0700

    Tabs are working

--- 3-creating-tabs/js/main.js
+++ 3-creating-tabs/js/main.js
@@ -20,12 +20,10 @@
      }
      this.setActive = function() {
        this.isActive = true;
-       console.log(this.elem);
        this.elem.classList.add("active");
      }
      this.isActive = isActive;
      this.elem = document.querySelector(id);
-     console.log(this.elem);
      this.id = id;
   }
 
@@ -35,15 +33,17 @@
           tempTab,
           tempContent;
       for (i = 0; i < config.length; i++) {
-        tempTab = new Tab(config[i].id, config[i].isActive);
         tempContent = new Content(config[i].content, config[i].isActive);
+        tempTab = new Tab(config[i].id, config[i].isActive, tempContent);
         if (tempTab.isActive) {
           this.currentTab = tempTab;
+        };
+        if (tempContent.isActive) {
+          this.currentContent = tempContent;
         };
       }
       this.tabClickListener();
     }  
-    this.options = optionsObj;
     this.init();
   }
 
@@ -52,18 +52,26 @@
       if (this.currentTab) {
         this.currentTab.removeActive();
       }
-      console.log(newCurrentTab);
       this.currentTab = newCurrentTab;
     },
     tabClickListener: function() {
       document.addEventListener("tabClick", function(e) {
         this.setCurrentTab(e.detail);
+        this.setCurrentContent(e.detail.content);
       }.bind(this))
+    },
+    setCurrentContent: function(newContent) {
+      if (this.currentContent) {
+        this.currentContent.removeActive();
+      };
+      this.currentContent = newContent;
+      this.currentContent.setActive();
     }
   }
 
-  function Tab(id, isActive) {
+  function Tab(id, isActive, content) {
     DomActivator.call(this, id, isActive);
+    this.content = content;
     this.init = function() {
       this.isClicked();
       if (this.isActive) {
@@ -79,7 +87,6 @@
       var tabClick = new CustomEvent("tabClick", {"detail": this, bubbles: true});
       this.elem.addEventListener("click", function(){
         this.elem.dispatchEvent(tabClick);
-
         // Toggle Active Class on Tabs
         this.elem.classList.toggle("active");
 
